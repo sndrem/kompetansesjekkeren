10f550349ddf9b51d39688e575647360a9a7b798
Sindre Moldeklev <sndrem@gmail.com>
2019-09-02 06:43:22 +0200

    Fjernet build-mappen i frontend fra gitignore

---
+++ frontend/build/precache-manifest.f7b0d02806e0c2ace6b609de54410f34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "080fc933a98da769f9028b12be4492bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74e64b87658b1ed03ebf",
+    "url": "/static/css/main.28410a36.chunk.css"
+  },
+  {
+    "revision": "102971a9643312d24c3d",
+    "url": "/static/js/2.f503af1a.chunk.js"
+  },
+  {
+    "revision": "74e64b87658b1ed03ebf",
+    "url": "/static/js/main.fc04c392.chunk.js"
+  },
+  {
+    "revision": "b170bb97c411cdd9d66b",
+    "url": "/static/js/runtime~main.0b638f56.js"
+  }
+]);
