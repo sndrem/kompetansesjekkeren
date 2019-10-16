9461fb2483bb40c61ac4acbe482bf2245f953371
Sindre Moldeklev <sndrem@gmail.com>
2019-10-16 19:17:59 +0200

    Lagt til lenke for fagrådet for våtrom og mesterregisteret

--- frontend/src/components/sokefelt/sokefelt.tsx
+++ frontend/src/components/sokefelt/sokefelt.tsx
@@ -26,14 +26,14 @@ function Sokefelt(props: Props) {
     return (
         <Form className="sokeform" action="#" onSubmit={(e) => e.preventDefault()}>
             <Header as="h1">Kompetansesjekkeren</Header>
-            <p>Sjekk organisasjon opp mot <a target="_blank" rel="noopener noreferrer" href="https://www.brreg.no/">Brønnøysundregisteret (Enhetsregisteret)</a>, <a target="_blank" rel="noopener noreferrer" href="https://sgregister.dibk.no/">Sentral godkjenning</a> og Arbeidstilsynet sitt <a target="_blank" rel="noopener noreferrer" href="https://www.arbeidstilsynet.no/registre/renholdsregisteret/sok/">renholdsregister</a>.</p>
+            <p>Sjekk organisasjon opp mot <a target="_blank" rel="noopener noreferrer" href="https://www.brreg.no/">Brønnøysundregisteret (Enhetsregisteret)</a>, <a target="_blank" rel="noopener noreferrer" href="https://sgregister.dibk.no/">Sentral godkjenning</a> <a target="_blank" rel="noopener noreferrer" href="http://www.ffv.no/">Fagrådet for våtrom</a>, <a target="_blank" rel="noopener noreferrer" href="https://www.mesterbrev.no/sok-mesterregisteret/">Mesterbrevsregisteret</a> og Arbeidstilsynet sitt <a target="_blank" rel="noopener noreferrer" href="https://www.arbeidstilsynet.no/registre/renholdsregisteret/sok/">renholdsregister</a>.</p>
             {feilmelding && <Message negative>{feilmelding}</Message>}
             <Form.Field>
                 <label>Søk på organisasjonsnummer</label>
                 <input placeholder="Organisasjonsnummer - 9 siffer" value={orgnr} onChange={(e) => setOrgnr(removeSpaces(e.currentTarget.value))} type="text" id="orgnr" aria-describedby="orgnr-sok" />
             </Form.Field>
             <Button primary className="cta-btn" onClick={() => validerInput(orgnr)}>Søk</Button>
-        </Form>
+        </Form >
     )
 }
 
