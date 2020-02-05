ef0864768cb8a6134f7f4ab794a3d5bc4889a64a
Sindre Moldeklev <sndrem@gmail.com>
2020-02-05 20:25:27 +0100

    Oppdaterer scrapejobb med cron-uttrykk for kl. 07.00

--- cron-jobs/scrape-job.js
+++ cron-jobs/scrape-job.js
@@ -2,3 +2,4 @@ const cron = require("node-cron");
 const scrape = require("../scraper/scraper");
 
 cron.schedule("0 12,0 * * *", scrape.scrapeAndPopulateDb);
+cron.schedule("0 7 * * *", scrape.scrapeAndPopulateDb);
