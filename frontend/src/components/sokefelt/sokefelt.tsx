278e5ff37be1ec79cae97797957a53b0e0e5fd61
Sindre Moldeklev <sindre.moldeklev@bekk.no>
2020-03-17 21:33:05 +0100

    Lagt til animasjon på søkeknapp

--- frontend/src/components/sokefelt/sokefelt.tsx
+++ frontend/src/components/sokefelt/sokefelt.tsx
@@ -1,40 +1,109 @@
-import React, { useState } from 'react';
-import { Form, Header, Button, Message } from 'semantic-ui-react';
+import React, { useState } from "react";
+import { Form, Header, Message } from "semantic-ui-react";
+import { motion } from "framer-motion";
+import styled from "styled-components";
 
 interface Props {
-    onSubmit: (orgnr: string) => void;
+  onSubmit: (orgnr: string) => void;
 }
+
+const Knapp = styled(motion.button)`
+  background: #2185d0;
+  color: white;
+  padding: 1rem;
+  border-radius: 15px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 function Sokefelt(props: Props) {
-    const [orgnr, setOrgnr] = useState("");
-    const [feilmelding, setFeilmelding] = useState("");
+  const [orgnr, setOrgnr] = useState("");
+  const [feilmelding, setFeilmelding] = useState("");
 
-    function removeSpaces(value: string): string {
-        return value.replace(/\W/g, "");
+  function removeSpaces(value: string): string {
+    return value.replace(/\W/g, "");
+  }
+
+  function validerInput(orgnr: string) {
+    const spacesRemoved = removeSpaces(orgnr);
+    if (spacesRemoved.length > 0 && spacesRemoved.length < 9) {
+      setFeilmelding("Organisasjonsnummer må være 9 siffer");
+    } else {
+      setFeilmelding("");
+      props.onSubmit(orgnr);
     }
+  }
 
-    function validerInput(orgnr: string) {
-        const spacesRemoved = removeSpaces(orgnr);
-        if (spacesRemoved.length > 0 && spacesRemoved.length < 9) {
-            setFeilmelding("Organisasjonsnummer må være 9 siffer");
-        } else {
-            setFeilmelding("");
-            props.onSubmit(orgnr);
-        }
-    }
-
-    return (
-        <Form className="sokeform" action="#" onSubmit={(e) => e.preventDefault()}>
-            <Header as="h1">Kompetansesjekkeren</Header>
-            <p>Sjekk organisasjon opp mot <a target="_blank" rel="noopener noreferrer" href="https://www.brreg.no/">Brønnøysundregisteret (Enhetsregisteret)</a>, <a target="_blank" rel="noopener noreferrer" href="https://sgregister.dibk.no/">Sentral godkjenning</a> <a target="_blank" rel="noopener noreferrer" href="http://www.ffv.no/">Fagrådet for våtrom</a>, <a target="_blank" rel="noopener noreferrer" href="https://www.mesterbrev.no/sok-mesterregisteret/">Mesterbrevsregisteret</a> og Arbeidstilsynet sitt <a target="_blank" rel="noopener noreferrer" href="https://www.arbeidstilsynet.no/registre/renholdsregisteret/sok/">renholdsregister</a>.</p>
-            {feilmelding && <Message negative>{feilmelding}</Message>}
-            <Form.Field className="">
-                <label>Søk på organisasjonsnummer</label>
-                <input placeholder="Organisasjonsnummer - 9 siffer" value={orgnr} onChange={(e) => setOrgnr(removeSpaces(e.currentTarget.value))} type="text" id="orgnr" aria-describedby="orgnr-sok" />
-            </Form.Field>
-            <Button primary className="cta-btn" onClick={() => validerInput(orgnr)}>Søk</Button>
-        </Form >
-    )
+  return (
+    <Form className="sokeform" action="#" onSubmit={e => e.preventDefault()}>
+      <Header as="h1">Kompetansesjekkeren</Header>
+      <p>
+        Sjekk organisasjon opp mot{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.brreg.no/"
+        >
+          Brønnøysundregisteret (Enhetsregisteret)
+        </a>
+        ,{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://sgregister.dibk.no/"
+        >
+          Sentral godkjenning
+        </a>{" "}
+        <a target="_blank" rel="noopener noreferrer" href="http://www.ffv.no/">
+          Fagrådet for våtrom
+        </a>
+        ,{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.mesterbrev.no/sok-mesterregisteret/"
+        >
+          Mesterbrevsregisteret
+        </a>{" "}
+        og Arbeidstilsynet sitt{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.arbeidstilsynet.no/registre/renholdsregisteret/sok/"
+        >
+          renholdsregister
+        </a>
+        .
+      </p>
+      {feilmelding && <Message negative>{feilmelding}</Message>}
+      <Form.Field className="">
+        <label>Søk på organisasjonsnummer</label>
+        <input
+          placeholder="Organisasjonsnummer - 9 siffer"
+          value={orgnr}
+          onChange={e => setOrgnr(removeSpaces(e.currentTarget.value))}
+          type="text"
+          id="orgnr"
+          aria-describedby="orgnr-sok"
+        />
+      </Form.Field>
+      <Knapp
+        whileHover={{ scale: 1.1 }}
+        transition={spring}
+        onClick={() => validerInput(orgnr)}
+      >
+        Søk
+      </Knapp>
+    </Form>
+  );
 }
+
+const spring = {
+  type: "spring",
+  damping: 10,
+  stiffness: 100
+};
 
 export default Sokefelt;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as Sentry from '@sentry/browser';
+import * as Sentry from "@sentry/browser";
 import App from "./App";
 import "./index.scss";
 
 if (process.env.NODE_ENV === "production") {
-    Sentry.init({ dsn: "https://33ed86d660434abeb0a866dc9d6d4e95@sentry.io/1725688" });
+  Sentry.init({
+    dsn: "https://33ed86d660434abeb0a866dc9d6d4e95@sentry.io/1725688"
+  });
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
