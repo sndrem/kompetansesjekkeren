a34fda266c464fc359e96d8d6bcbe684bc1dd766
Sindre Moldeklev <sndrem@gmail.com>
2022-05-07 16:10:43 +0200

    Legger til readme

--- frontend/src/services/slackService.ts
+++ frontend/src/services/slackService.ts
@@ -1,24 +1,26 @@
-import { Tilbakemelding } from './../types/domain';
+import { Tilbakemelding } from "./../types/domain";
 export function notifySlack(organisasjonsnummer: string) {
-    if (process.env.REACT_APP_MILJO !== "dev") {
-        fetch("/slack", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ organisasjonsnummer })
-        })
-    } else {
-        console.info("Sender ikke slack-notifikasjon ved utvikling");
-    }
+  if (process.env.REACT_APP_MILJO !== "dev") {
+    fetch("/slack", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ organisasjonsnummer }),
+    });
+  } else {
+    console.info("Sender ikke slack-notifikasjon ved utvikling");
+  }
 }
 
-export function sendTilbakemelding(tilbakemelding: Tilbakemelding): Promise<Response> {
-    return fetch("/slack/feedback", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(tilbakemelding)
-    })
+export function sendTilbakemelding(
+  tilbakemelding: Tilbakemelding
+): Promise<Response> {
+  return fetch("/slack/feedback", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(tilbakemelding),
+  });
 }
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Kompetansesjekkeren
+Her ligger koden for backend og frontend for kompetansesjekkeren. 
+
+## Kom i gang
+Klon repoet og kjør `npm run build`. Frontend vil da bygges. Kjør så `npm run dev` for å kjøre opp løsningen lokalt.
