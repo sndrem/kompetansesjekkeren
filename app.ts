e995493b65dba19fc79b07c2d5b283aa68375ae1
Sindre Moldeklev <sndrem@gmail.com>
2023-03-26 12:40:49 +0200

    dist/frontend/build

--- app.ts
+++ app.ts
@@ -31,7 +31,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "frontend/build")));
+app.use(express.static(path.join(__dirname, "dist/frontend/build")));
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Listening on port: ${PORT}`);
